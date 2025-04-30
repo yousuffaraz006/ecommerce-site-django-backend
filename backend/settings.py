d5ada579c210bdfbd63b9fce70de08eecdf6e369
yousuffaraz006 <yousuffaraz006@gmail.com>
2025-04-30 08:40:35 +0530

    Deployment Changes

--- backend/settings.py
+++ backend/settings.py
@@ -180,5 +180,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    "https://shopsite-gamma.vercel.app",
 ]
